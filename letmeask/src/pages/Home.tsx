4f6472e76e02dbf0374fe699d76d69703527ec81
Jamilly Cuco <jamillysantos2507@gmail.com>
2021-07-03 22:04:45 -0300

    update home

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -79,7 +79,7 @@ export function Home(){
 
                     <form onSubmit={handleJoinRoom}>
                         <input type="text" 
-                        placeholder="Digite o cógico da sala"
+                        placeholder="Digite o código da sala"
                         onChange={event => setRoomCode(event.target.value)}
                         value = {roomCode}/>
                         <Button type="submit">
